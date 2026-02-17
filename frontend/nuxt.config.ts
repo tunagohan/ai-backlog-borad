2191425a27be71db818f69a85f6f44f1bbf7b021
tunagohan <kazutomo.waragai@andpad.co.jp>
2026-02-18 01:32:47 +0900

    add UI module

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,5 +5,6 @@ export default defineNuxtConfig({
     public: {
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api/v1'
     }
-  }
+  },
+  modules: ['@nuxt/ui']
 })
